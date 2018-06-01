4c010acfbfd206f88ebb08a55826c4b594165223
renzo <renzo.h.coronel@gmail.com>
2018-06-01 18:04:23 -0300

    * Agregado buttons

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,20 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container-fluid">
-    <div class="row "> <h1> CODAPLI </h1> </div>
+  <div class="container">
+    <div class="row "> 
+    <div class="col-md-12 text-center">
+      <h1> CODAPLI </h1>
+    </div>
+    </div>
 
-    <div class="row " style="height:500px;">
-      <div class="col-md-6">
+    <div class="row h-100">
+      <div class="col-md-6" style="height:240px">
         <app-joystick-container>  </app-joystick-container>
       </div>
-    <div class="col-md-6">
-       <app-joystick-container>  </app-joystick-container>
+    <div class="col-md-6 mh-100">
+       <div class="row pt-2 justify-content-center"> 
+            <div class="col-md-12 text-center"> 
+              <button class="btn btn-danger btn-circle btn-xl">Open </button>
+            </div>
+       </div>
+       <div class="row pt-4"> 
+            <div class="col-md-12 text-center"> 
+              <button class="btn btn-danger btn-circle btn-xl"> Close </button>
+            </div>
+       </div>
     </div>
     </div>
   </div>
-
-  `
+  `,
 })
 export class AppComponent {
   title = 'app';
